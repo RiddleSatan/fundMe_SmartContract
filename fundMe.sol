7d065c0b8d0582a839e4df2a49157a1f3c1a377e
RiddleSatan <mynameisompandey007@gmail.com>
2024-08-30 17:31:43 +0530

    modifier

--- fundMe.sol
+++ fundMe.sol
@@ -8,9 +8,14 @@ contract FundMe {
     using PriceConvertor for uint256; //this is important line to add inorder to use the function of the library we created which is created
 
     uint256 minimumUSD = 5e18;
+    address public owner;
 
     address[] public funders;
     mapping(address => uint256) public addressToAmountFunded;
+
+    constructor(){
+        owner=msg.sender;
+    }
 
     function fund() public payable {
         // function withdraw() public {}
@@ -22,7 +27,10 @@ contract FundMe {
         addressToAmountFunded[msg.sender] += msg.value;
     }
 
-    function withDraw() public {
+   
+
+    function withDraw() public onlyOwner /*onlyOwner is the Modifier that we created*/{
+        // require(owner==msg.sender,"Must be the Owner!");// bad practice better way is to use a modifier
         for (
             uint256 fundersIndex = 0;
             fundersIndex <= funders.length;
@@ -38,11 +46,11 @@ contract FundMe {
         //transfer :Limit of 2300 Gas sends an error if fails
         // ## msg.sender is of type Address
         // ## payable(msg.sender) is of type payable address
-        payable(msg.sender).transfer(address(this).balance);
+        // payable(msg.sender).transfer(address(this).balance);
 
         //send :Limit of 2300 Gas
-        bool sendSuccess = payable(msg.sender).send(address(this).balance);
-        require(sendSuccess, "there was an error: send failed"); //if sendSuccess is False then the function will revert else it will continue
+        // bool sendSuccess = payable(msg.sender).send(address(this).balance);
+        // require(sendSuccess, "there was an error: send failed"); //if sendSuccess is False then the function will revert else it will continue
 
         //call : no Limit Forwards all the Gas 
         (
@@ -50,5 +58,10 @@ contract FundMe {
 
         ) = payable(msg.sender).call{value: address(this).balance}("");
         require(callSuccess, "call failed");
+    }
+
+    modifier onlyOwner(){// so a modifer is something that we can use to create a functionality and then add that functionality to anyfunction we create 
+        require(owner==msg.sender,"You are not the Owner");
+        _;// this denotes basically the rest of your code and it means the he require methode above will execute first then the rest of the code if the conditon is satisfied else it will revert back
     }
 }
